71740cde048f55e845e5cc5893d98b044709d0b7
zm16555 <zm16555@josai.ac.jp>
2019-02-04 09:22:27 -0500

    message

--- ex0-1.js
+++ ex0-1.js
@@ -2,3 +2,13 @@ function sayhello71(){
   alert('Hello,'+
        document.getElementById('who71').value+'!');
 }
+function ThreeSecChange(){
+
+  setInterval("imgchange12()",3000);}
+
+
+function imgchange12(){
+
+  document.getElementById('logo2').src ='./image/melon.png';
+
+}
